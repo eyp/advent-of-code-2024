7a177753d2bac4fe38cfa421b00058026daf8612
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2024-12-03 16:10:14 +0100

    day one

---
+++ day-01/problem-1_1.exs
@@ -0,0 +1,40 @@
+defmodule AdventOfCode do
+  def solve(filename) do
+    File.read!(filename)
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.reduce({[],[]}, fn {left, right}, {left_acc, right_acc} ->
+      {
+        [left | left_acc],
+        [right | right_acc]
+      }
+    end)
+    |> then(fn {left, right} ->
+      {Enum.sort(left), Enum.sort(right)}
+    end)
+    |> calculate_distance()
+  end
+
+defp parse_line(line) do
+    case String.split(line) do
+      [left, right] ->
+        {String.to_integer(left), String.to_integer(right)}
+      _ ->
+        nil
+    end
+  end
+
+defp calculate_distance({left_numbers, right_numbers}) do
+    Enum.zip_reduce(left_numbers, right_numbers, 0, fn left, right, acc ->
+        sum = abs(right - left)
+        acc + sum
+    end)
+  end
+end
+
+distance = AdventOfCode.solve("input.txt")
+
+IO.puts(distance)
+
+
---
+++ day-01/problem-1_2.exs
@@ -0,0 +1,35 @@
+defmodule AdventOfCode do
+  def solve(filename) do
+    File.read!(filename)
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.reduce({[],[]}, fn {left, right}, {left_acc, right_acc} ->
+      {
+        [left | left_acc],
+        [right | right_acc]
+      }
+    end)
+    |> then(fn {left, right} -> 
+      frequencies = Enum.frequencies(right)
+      Enum.map(left, fn num -> {num, Map.get(frequencies, num, 0)} end)
+    end)
+    |> Enum.map(fn {left, right} -> left * right end)
+    |> Enum.sum()
+  end
+
+  defp parse_line(line) do
+    case String.split(line) do
+      [left, right] ->
+        {String.to_integer(left), String.to_integer(right)}
+      _ ->
+        nil
+    end
+  end
+end
+
+similarity = AdventOfCode.solve("input.txt")
+
+IO.puts(similarity)
+
+
