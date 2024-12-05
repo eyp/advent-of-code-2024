56c931e98db40d9b1dbe6c1b68b3719d00a87d4b
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2024-12-05 14:23:23 +0100

    Renamed funcion, pipe it in the main pipeline

--- day-03/problem-3_2.exs
+++ day-03/problem-3_2.exs
@@ -6,20 +6,19 @@ defmodule AdventOfCode do
     |> Enum.reject(&String.length(&1) == 0)
     |> Enum.map(&Regex.scan(~r/mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)/, &1))
     |> List.flatten()
-
-    sum_list(list, 0, true)
+    |> calculate_result(0, true)
   end
 
-  defp sum_list([head | tail], accumulator, multiplying) do
+  defp calculate_result([head | tail], accumulator, multiplying) do
     IO.puts("head: #{head}")
     IO.puts("tail: #{tail}")
     IO.puts("accumulator: #{accumulator}")
     IO.puts("multiplying: #{multiplying}")
     cond do
       Regex.match?(~r/don't\(\)/, head) ->
-        sum_list(tail, accumulator, false)
+        calculate_result(tail, accumulator, false)
       Regex.match?(~r/do\(\)/, head) ->
-        sum_list(tail, accumulator, true)
+        calculate_result(tail, accumulator, true)
       true ->
         IO.puts("processing multiplication...")
         if multiplying do
@@ -29,15 +28,15 @@ defmodule AdventOfCode do
           |> Enum.map(&String.split(&1, ","))
           |> Enum.flat_map(fn [x, y] -> [String.to_integer(x) * String.to_integer(y)] end)
           |> Enum.sum()
-          sum_list(tail, accumulator + result, multiplying)
+          calculate_result(tail, accumulator + result, multiplying)
         else
           IO.puts("multiplying disabled")
-          sum_list(tail, accumulator, multiplying)
+          calculate_result(tail, accumulator, multiplying)
         end
       end
   end
 
-  defp sum_list([], accumulator, multiplying) do
+  defp calculate_result([], accumulator, multiplying) do
     accumulator
   end
 end
